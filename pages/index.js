a15cd834b53a5f046c47efb5e38adac207d1e006
lanzafame <adrianlanzafame92@gmail.com>
2023-08-14 12:41:06 +1000

    edit index

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
 
 import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
+// import Video from "../components/video";
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
-import Testimonials from "../components/testimonials";
+// import Testimonials from "../components/testimonials";
 import Cta from "../components/cta";
 import Faq from "../components/faq";
 import PopupWidget from "../components/popupWidget";
@@ -16,10 +16,10 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+        <title>Skapa - Web3 Infrastructure Consulting</title>
         <meta
           name="description"
-          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
+          content="Unlock the power of web3 for your business. Dive into the decentralized future with Skapa."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -35,21 +35,6 @@ const Home = () => {
       </SectionTitle>
       <Benefits data={benefitOne} />
       <Benefits imgPos="right" data={benefitTwo} />
-      <SectionTitle
-        pretitle="Watch a video"
-        title="Learn how to fullfil your needs">
-        This section is to highlight a promo or demo video of your product.
-        Analysts says a landing page with video has 3% more conversion rate. So,
-        don&apos;t forget to add one. Just like this.
-      </SectionTitle>
-      <Video />
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said">
-        Testimonails is a great way to increase the brand trust and awareness.
-        Use this section to highlight your popular customers.
-      </SectionTitle>
-      <Testimonials />
       <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
         Answer your customers possible questions here, it will increase the
         conversion rate as well as support or chat requests.
@@ -61,5 +46,24 @@ const Home = () => {
     </>
   );
 }
+
+
+// Video section for later use.
+      // <SectionTitle
+      //   pretitle="Watch a video"
+      //   title="Learn how to fullfil your needs">
+      //   This section is to highlight a promo or demo video of your product.
+      //   Analysts says a landing page with video has 3% more conversion rate. So,
+      //   don&apos;t forget to add one. Just like this.
+      // </SectionTitle>
+      // <Video />
+// Testimonial section for later use.
+      // <SectionTitle
+      //   pretitle="Testimonials"
+      //   title="Here's what our customers said">
+      //   Testimonails is a great way to increase the brand trust and awareness.
+      //   Use this section to highlight your popular customers.
+      // </SectionTitle>
+      // <Testimonials />
 
 export default Home;
